deef07e10528837d70486ea26f974ccc5297db8e
Noy Moyal <noymoyyal@gmail.com>
2026-08-14 14:41:17 +0300

    Load and display notes count in NoteIndex

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,5 +1,20 @@
+const { useState, useEffect } = React
+
+
+
 import { noteService } from '../services/note.service.js'
 export function NoteIndex() {
-    return <section className="container">Notes app</section>
+        const [notes, setNotes] = useState(null)
+
+    useEffect(() => {
+        loadNotes()
+    }, [])
+
+    function loadNotes() {
+        noteService.query().then(setNotes)
+    }
+
+    if (!notes) return <div>Loading...</div>
+    return <section className="container">I have {notes.length} notes</section>
 }
 
