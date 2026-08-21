c30c41a9a12f27dcf2834b298450549c131e4144
Noy Moyal <noymoyyal@gmail.com>
2026-08-22 00:32:27 +0300

    Add edit mode with editable rows to todos note

--- apps/note/cmps/NoteTodos.jsx
+++ apps/note/cmps/NoteTodos.jsx
@@ -8,6 +8,62 @@ export function NoteTodos({ info, isEditMode, onChangeInfo }) {
     onChangeInfo({ target: { name: "todos", value: updatedTodos } })
   }
 
+  function onChangeTodoTxt(todoIdx, txt) {
+    const updatedTodos = todos.map((todo, idx) =>
+      idx === todoIdx ? { ...todo, txt } : todo,
+    )
+    onChangeInfo({ target: { name: "todos", value: updatedTodos } })
+  }
+
+  function onAddTodo() {
+    const updatedTodos = [...todos, { txt: "", isDone: false }]
+    onChangeInfo({ target: { name: "todos", value: updatedTodos } })
+  }
+
+  function onRemoveTodo(todoIdx) {
+    const updatedTodos = todos.filter((todo, idx) => idx !== todoIdx)
+    onChangeInfo({ target: { name: "todos", value: updatedTodos } })
+  }
+
+  if (isEditMode) {
+    return (
+      <div className="note-todos-edit">
+        <input
+          type="text"
+          name="title"
+          value={info.title}
+          onChange={onChangeInfo}
+          placeholder="Title"
+        />
+               {todos.map((todo, idx) => (
+          <div className="todo-row" key={idx}>
+            <input
+              type="checkbox"
+              checked={todo.isDone}
+              onChange={() => onToggleTodo(idx)}
+            />
+            <input
+              type="text"
+              value={todo.txt}
+              onChange={(ev) => onChangeTodoTxt(idx, ev.target.value)}
+            />
+            <button
+              type="button"
+              onClick={() => onRemoveTodo(idx)}
+              aria-label="Remove item"
+            >
+              <i className="fa-solid fa-xmark"></i>
+            </button>
+          </div>
+        ))}
+
+        <button type="button" onClick={onAddTodo} className="add-todo">
+          <i className="fa-solid fa-plus"></i> List item
+        </button>
+      </div>
+    )
+  }
+
   return (
     <div className="note-todos">
       <h3>{info.title}</h3>
